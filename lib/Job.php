d125a46f83ca2d6f7dfdb4a108653465bc7013b3
Imon <contact@imonweb.co.uk>
2024-07-20 10:15:57 +0100

    category

--- lib/Job.php
+++ lib/Job.php
@@ -11,7 +11,7 @@ class Job {
   // Get all Jobs
   public function getAllJobs()
   {
-    /*
+    
     $this->db->query("SELECT jobs.*, categories.name AS cname 
 						FROM jobs 
 						INNER JOIN categories
@@ -20,11 +20,54 @@ class Job {
 						");
           // Assign result set 
           $results = $this->db->resultSet();
-    */
-     $this->db->query("SELECT * FROM jobs ORDER BY id");
+    
+    // $this->db->query("SELECT * FROM jobs ORDER BY id");
+    // Assign result set 
+    $results = $this->db->resultSet();
+
+    return $results;
+  }
+
+  // Get Categories
+  public function getCategories()
+  {
+    $this->db->query("SELECT * FROM categories");
+    // Assign result set 
+    $results = $this->db->resultSet();
+
+    return $results;
+  }
+
+  // Get Jobs by Category 
+  public function getByCategory($category)
+  {
+    $this->db->query("SELECT jobs.*, categories.name AS cname 
+						FROM jobs 
+						INNER JOIN categories
+						ON jobs.category_id = categories.id 
+            WHERE jobs.category_id = $category
+						ORDER BY post_date DESC 
+						");
           // Assign result set 
           $results = $this->db->resultSet();
+    
+    $results = $this->db->resultSet();
 
-          return $results;
+    return $results;
   }
+
+  // Get category
+  public function getCategory($category_id)
+  {
+    $this->db->query("SELECT * FROM categories WHERE id = :category_id");
+    $this->db->bind(':category_id', $category_id);
+
+    // Assign Row
+    $row = $this->db->single();
+
+    return $row;
+  }
+
+
+
 } // class Job
--- templates/frontpage.php
+++ templates/frontpage.php
@@ -4,9 +4,9 @@
         <form method="GET" action="index.php">
             <select name="category" class="form-control">
                 <option value="0">Choose Category</option>
-                <?php // foreach($categories as $category): ?>
-                    <option value="<?php // echo $category->id; ?>"><?php // echo $category->name; ?></option>
-                <?php // endforeach; ?>
+                <?php foreach($categories as $category): ?>
+                    <option value="<?php echo $category->id; ?>"><?php echo $category->name; ?></option>
+                <?php endforeach; ?>
             </select>
             <br>
             <input type="submit" class="btn btn-lg btn-success" value="FIND">
