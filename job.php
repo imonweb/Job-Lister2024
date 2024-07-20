65a0e4cb5e4a9552e4034c97e8f6cd5c87a6ed6a
Imon <contact@imonweb.co.uk>
2024-07-20 22:55:05 +0100

    create job

---
+++ job.php
@@ -0,0 +1,17 @@
+<?php include_once 'config/init.php'; ?>
+<?php
+$job = new Job;
+
+$template = new Template('templates/job-single.php');
+
+$job_id = isset($_GET['id']) ? $_GET['id'] : null;
+
+$template->job  = $job->getJob($job_id);
+
+echo $template;
+
+// var_dump($template);
+
+// echo '<pre>';
+// print_r($template);
+// echo '</pre>';
